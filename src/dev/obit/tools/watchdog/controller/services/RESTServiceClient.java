e457e4061e683cad9d7bc2b18f57e80c5b0956bc
seb <git@00b.it>
2022-01-27 15:03:10 +0100

    added latency check

--- src/dev/obit/tools/watchdog/controller/services/RESTServiceClient.java
+++ src/dev/obit/tools/watchdog/controller/services/RESTServiceClient.java
@@ -88,7 +88,9 @@ public class RESTServiceClient extends ScheduledService<NetStatus>{
     	try {
  
     		start = System.currentTimeMillis();
-    		response = Jsoup.connect(targetDomain+targetProduct)
+//    		response = Jsoup.connect(targetDomain+targetProduct)
+//      				.execute();
+                response = Jsoup.connect(targetDomain+targetProduct)
     				.userAgent("Mozilla/5.0 (Windows NT x.y; Win64; x64; rv:10.0) Gecko/20100101 Firefox/10.0")
     				.timeout(timeOutPeriod)
     				.referrer("http://google.com")
