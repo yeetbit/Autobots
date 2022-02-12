97acf1b2670a3be362f5b9733cd7bc26075c8e30
seb <git@00b.it>
2022-02-12 13:49:13 +0100

    updated module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module dev.obit.watchdog {
     requires javafx.controls;
     requires javafx.fxml;
+	requires org.jsoup;
+	requires org.slf4j;
 
     opens dev.obit.watchdog.view to javafx.fxml;
     exports dev.obit.watchdog;
